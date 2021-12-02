689f5f09bd50e27922566e5f6fa11fbfb148d607
SixSafe <suphakon043@gmail.com>
2021-12-02 16:33:13 +0700

    demo

--- day12/showdemo.php
+++ day12/showdemo.php
@@ -6,9 +6,11 @@ $dbname = "it64309010017";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
+mysqli_set_charset($conn,"utf8");
 // Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
+  
 }
 
 $sql = "SELECT * FROM demo";
